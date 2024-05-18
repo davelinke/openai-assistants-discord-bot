82c13bf43995adb30ed18ea68e3ac44cd0d34994
David Linke <davelinke@users.noreply.github.com>
2024-05-18 08:53:02 -0400

    Modified bot to split messages

--- bot.js
+++ bot.js
@@ -1,18 +1,24 @@
-const { Client, GatewayIntentBits } = require('discord.js');
+const { Client, GatewayIntentBits, Partials } = require('discord.js');
 const { OpenAI } = require("openai");
 require("dotenv").config();
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY
+    apiKey: process.env.OPENAI_API_KEY,
+    defaultHeaders: { "OpenAI-Beta": "assistants=v2" }
 });
 
 // Discord Client
 const client = new Client({
-  intents: [
+    intents: [
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.MessageContent
-    ]
+        GatewayIntentBits.MessageContent,
+        GatewayIntentBits.DirectMessages,
+    ],
+    partials: [
+        Partials.Channel,
+        Partials.Message,
+    ],
 });
 
 const sleep = (ms) => {
@@ -43,7 +49,7 @@ const statusCheckLoop = async (openAiThreadId, runId) => {
         runId
     );
 
-    if(terminalStates.indexOf(run.status) < 0){
+    if (terminalStates.indexOf(run.status) < 0) {
         await sleep(1000);
         return statusCheckLoop(openAiThreadId, runId);
     }
@@ -60,19 +66,79 @@ const addMessage = (threadId, content) => {
     )
 }
 
+/**
+ * Splits a Markdown string into chunks of specified max length while maintaining formatting,
+ * ensuring code blocks are handled correctly.
+ * @param {string} markdown - The Markdown string to be split.
+ * @param {number} maxLength - The maximum length of each chunk.
+ * @returns {string[]} - An array of formatted chunks.
+ */
+function splitMarkdownToChunks(markdown, maxLength = 1999) {
+    const chunks = [];
+    let currentChunk = '';
+    let inCodeBlock = false;
+    const lines = markdown.split('\n');
+
+    lines.forEach((line) => {
+        // Check if the line starts or ends a code block
+        if (line.startsWith('```')) {
+            inCodeBlock = !inCodeBlock;
+        }
+
+        // Check if adding the line would exceed the max length
+        if ((currentChunk + '\n' + line).length <= maxLength) {
+            currentChunk += (currentChunk ? '\n' : '') + line;
+        } else {
+            // Push the current chunk and start a new one
+            if (inCodeBlock && !line.startsWith('```')) {
+                // Close the current code block and open a new one in the next chunk
+                currentChunk += '\n```';
+                chunks.push(currentChunk);
+                currentChunk = '```' + '\n' + line;
+            } else {
+                chunks.push(currentChunk);
+                currentChunk = line;
+            }
+
+            // Ensure the new chunk itself is not too large
+            if (currentChunk.length > maxLength) {
+                while (currentChunk.length > maxLength) {
+                    chunks.push(currentChunk.slice(0, maxLength));
+                    currentChunk = currentChunk.slice(maxLength);
+                }
+            }
+        }
+    });
+
+    // Push any remaining content in the current chunk
+    if (currentChunk) {
+        // If we're in a code block, make sure to close it
+        if (inCodeBlock && !currentChunk.endsWith('```')) {
+            currentChunk += '\n```';
+        }
+        chunks.push(currentChunk);
+    }
+
+    return chunks;
+}
+
+
 // This event will run every time a message is received
 client.on('messageCreate', async message => {
+    // console.log(message.type);
     if (message.author.bot || !message.content || message.content === '') return; //Ignore bot messages
     // console.log(message);
+    // sent typing indicator
+    message.channel.sendTyping();
     const discordThreadId = message.channel.id;
     let openAiThreadId = getOpenAiThreadId(discordThreadId);
 
     let messagesLoaded = false;
-    if(!openAiThreadId){
+    if (!openAiThreadId) {
         const thread = await openai.beta.threads.create();
         openAiThreadId = thread.id;
         addThreadToMap(discordThreadId, openAiThreadId);
-        if(message.channel.isThread()){
+        if (message.channel.isThread()) {
             //Gather all thread messages to fill out the OpenAI thread since we haven't seen this one yet
             const starterMsg = await message.channel.fetchStarterMessage();
             const otherMessagesRaw = await message.channel.messages.fetch();
@@ -91,7 +157,7 @@ client.on('messageCreate', async message => {
     }
 
     // console.log(openAiThreadId);
-    if(!messagesLoaded){ //If this is for a thread, assume msg was loaded via .fetch() earlier
+    if (!messagesLoaded) { //If this is for a thread, assume msg was loaded via .fetch() earlier
         await addMessage(openAiThreadId, message.content);
     }
 
@@ -103,11 +169,18 @@ client.on('messageCreate', async message => {
 
     const messages = await openai.beta.threads.messages.list(openAiThreadId);
     let response = messages.data[0].content[0].text.value;
-    response = response.substring(0, 1999) //Discord msg length limit when I was testing
-
     console.log(response);
-    
-    message.reply(response);
+    console.log('replying');
+
+    if (response.length < 1999) {
+        message.reply(response);
+    } else {
+        // split the response in chunks of 1999 characters and make sure you split at the new line character
+        const chunks = splitMarkdownToChunks(response, 1999)
+        for (let chunk of chunks) {
+            message.reply(chunk);
+        }
+    }
 });
 
 // Authenticate Discord
